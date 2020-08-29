a72ec89c920bc353b9ba4f59f1d68a67ca767274
Daniel Le <lequangduc512014@gmail.com>
2020-08-28 18:29:35 -0700

    initialize the folders

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const User = require('./Drink.js')
+const Income = require('./Bean.js')
+const Budget = require('./Syrup.js')
+const Activity = require('./Topping.js')
+
+
+
+module.exports = { User, Income, Budget, Activity }
