77b18dde88d115b0cc713ba1003d51fe30eb21f9
Daniel Le <lequangduc512014@gmail.com>
2020-09-01 20:20:42 -0700

    code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const { Activity, User } = require('../models')
 
 router.get('/users', (req, res) => {
+  console.log(res)
   User.findAll({ include: Activity })
     .then(users => res.json(users))
     .catch(err => console.log(err))
@@ -17,6 +18,15 @@ router.post('/users', (req, res) => {
     })
     .catch(err => console.log(err))
 })
+
+router.get('/users/:name', (req, res) => {
+  console.log(res)
+  User.findOne({ where: { name: req.params.name }, include: Activity })
+    .then(user => res.json(user)
+    )
+    .catch(err => console.log(err))
+})
+
 
 module.exports = router
 
