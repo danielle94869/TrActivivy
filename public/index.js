89bc2ece439d65673ce7777efd35274ceef438bb
Daniel Le <lequangduc512014@gmail.com>
2020-09-01 21:54:11 -0700

    code

--- public/index.js
+++ public/index.js
@@ -13,6 +13,7 @@ document.getElementById('create').addEventListener('click', event => {
     .catch(err => console.log(err))
 })
 
+// module.exports = User
 
 // document.getElementById('addGrocery').addEventListener('click', event => {
 //   event.preventDefault()
